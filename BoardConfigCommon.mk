f7b40ca93b784cea90ba9283f1528973e6debb7e
Michael Bestas <mkbestas@lineageos.org>
2019-09-26 01:56:06 +0300

    msm8953-common: Switch to device/qcom/sepolicy-legacy-um policy
    Change-Id: I09ee12755ab0a1f54a9dca4b92313ef1127db3d6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,7 +199,7 @@ TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab.recovery.qcom
 endif
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 SELINUX_IGNORE_NEVERALLOWS := true
 
