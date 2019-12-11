b6c025c1a5eefead7622a40e45beeada379cac37
Amar Bajpai <bajpai.amar3@gmail.com>
2019-12-11 17:20:12 +0530

    Revert "Build ThermalController"
    This reverts commit 4795796b7cf54de75bc891a385fc07edfd6b4d74.

--- msm8953.mk
+++ msm8953.mk
@@ -357,10 +357,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
-# ThermalController app
-PRODUCT_PACKAGES += \
-    ThermalController
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
